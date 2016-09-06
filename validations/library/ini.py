8502c3ad51787838c120d347266fd29324011aca
Tomas Sedovic <tsedovic@redhat.com>
2016-09-06 12:04:38 +0200

    Make the ini-based validations more robust
    They were failing when some of the config files they were checking
    didn't exist even though that shouldn't be a problem in these particular
    cases.
    
    So this updates the ini module to handle missing files, ini sections and
    values. In addition, the `failed_when` check is simplified.
    
    Change-Id: I6c0fb3ba8b16c7a442f764479b7e3c1703e5939e
    Closes-Bug: 1620549

--- validations/library/ini.py
+++ validations/library/ini.py
@@ -36,24 +36,40 @@ def main():
         path=dict(required=True, type='str'),
         section=dict(required=True, type='str'),
         key=dict(required=True, type='str'),
+        ignore_missing_file=dict(required=False, type='bool'),
     ))
 
     ini_file_path = module.params.get('path')
 
     if path.exists(ini_file_path) and path.isfile(ini_file_path):
         config = ConfigParser.SafeConfigParser()
-        config.read(ini_file_path)
-
         try:
-            value = config.get(module.params.get('section'),
-                               module.params.get('key'))
-        except ConfigParser.Error as e:
-            module.fail_json(msg=e.message)
+            config.read(ini_file_path)
+        except Exception:
+            module.fail_json(msg="The file '{}' is not in a valid INI format."
+                             .format(ini_file_path))
 
-        module.exit_json(changed=False, value=value)
+        section = module.params.get('section')
+        key = module.params.get('key')
+        try:
+            value = config.get(section, key)
+            msg = ("The key '{}' under the section '{}' in file {} "
+                   "has the value: '{}'"
+                   .format(key, section, ini_file_path, value))
+            module.exit_json(msg=msg, changed=False, value=value)
+        except ConfigParser.Error:
+            msg = ("There is no key '{}' under the section '{}' in file {}."
+                   .format(key, section, ini_file_path))
+            module.exit_json(msg=msg, changed=False, value=None)
+
     else:
-        module.fail_json(msg="Could not open the ini file: '{}'"
-                         .format(ini_file_path))
+        missing_file_message = "Could not open the ini file: '{}'".format(
+            ini_file_path)
+        if module.params.get('ignore_missing_file'):
+            module.exit_json(msg=missing_file_message, changed=False,
+                             value=None)
+        else:
+            module.fail_json(msg=missing_file_message)
 
 
 if __name__ == '__main__':
