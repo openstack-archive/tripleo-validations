a85fdc643ba144d4a5f2031dfb40c0b6456507ec
Katerina Pilatova <kpilatov@redhat.com>
2017-04-18 15:08:24 +0200

    Unit testing for undercloud_conf
    - modifies custom module undercloud_conf.py in such a way that its individual
      stages can be easily tested
    - tests for this module (test_undercloud_conf.py) are created
    
    Change-Id: I65951cd9bc864a731d485a94bf468eab46e7e2a0
    Partial-Bug: #1594785

--- validations/library/undercloud_conf.py
+++ validations/library/undercloud_conf.py
@@ -14,10 +14,52 @@
 # License for the specific language governing permissions and limitations
 # under the License.
 
-import ConfigParser
+try:
+    import configparser as ConfigParser
+except ImportError:
+    import ConfigParser
+
 from os import path
 
 from ansible.module_utils.basic import *  # noqa
+
+
+def check_arguments(undercloud_conf_path, ignore_missing):
+    '''Validate format of arguments
+
+    return: (True, errors) if file can be opened
+            (False, errors) if ignore_missing or check failed
+    '''
+
+    errors = []
+
+    if path.exists(undercloud_conf_path) and path.isfile(undercloud_conf_path):
+        return (True, errors)
+    else:
+        if not ignore_missing:
+            errors.append('Could not open the undercloud.conf file at {}'
+                          .format(undercloud_conf_path))
+        return (False, errors)
+
+
+def get_result(undercloud_conf_path):
+    '''Get result from undercloud file'''
+
+    result = {}
+    errors = []
+
+    try:
+        config = ConfigParser.SafeConfigParser()
+        config.read(undercloud_conf_path)
+    except ConfigParser.MissingSectionHeaderError:
+        return (['File contains no section headers.'], {})
+    except ConfigParser.ParsingError:
+        return (['File contains parsing errors.'], {})
+
+    sections = ['DEFAULT'] + config.sections()
+    result = dict(((section, dict(config.items(section)))
+                   for section in sections))
+    return (errors, result)
 
 
 def main():
@@ -27,24 +69,25 @@ def main():
     ))
 
     undercloud_conf_path = module.params.get('undercloud_conf_path')
+    ignore_missing = module.params.get('ignore_missing')
 
-    if path.exists(undercloud_conf_path) and path.isfile(undercloud_conf_path):
-        config = ConfigParser.SafeConfigParser()
-        config.read(undercloud_conf_path)
+    # Check arguments
+    file_exists, errors = check_arguments(undercloud_conf_path, ignore_missing)
 
-        sections = ['DEFAULT'] + config.sections()
-        result = dict(((section, dict(config.items(section)))
-                       for section in sections))
+    if errors:
+        module.fail_json(msg='\n'.join(errors))
+    elif file_exists:
+        # Get result
+        errors, result = get_result(undercloud_conf_path)
 
-        module.exit_json(changed=False,
-                         ansible_facts={u'undercloud_conf': result})
-    elif module.params.get('ignore_missing'):
+        if errors:
+            module.fail_json(msg='\n'.join(errors))
+        else:
+            module.exit_json(changed=False,
+                             ansible_facts={u'undercloud_conf': result})
+    else:
         module.exit_json(changed=False,
                          ansible_facts={u'undercloud_conf': {'DEFAULT': {}}})
-    else:
-        module.fail_json(msg="Could not open the undercloud.conf file at '%s'"
-                         % undercloud_conf_path)
-
 
 if __name__ == '__main__':
     main()
