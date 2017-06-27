97100af43ee4fdba90b6a1d01cf13ae07375f56d
Emilien Macchi <emilien@redhat.com>
2017-06-27 16:38:04 -0400

    Switch from oslosphinx to openstackdocstheme
    As part of the docs migration work[0] for Pike we need to switch to use the
    openstackdocstheme.
    
    [0] https://review.openstack.org/#/c/472275/
    
    Change-Id: I743ec1ab5ae83ee4e2a6da3b8803ea4aaed12082

--- releasenotes/source/conf.py
+++ releasenotes/source/conf.py
@@ -32,7 +32,7 @@
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'oslosphinx',
+    'openstackdocstheme',
     'reno.sphinxext',
 ]
 
@@ -125,7 +125,7 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'default'
+html_theme = 'openstackdocs'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -134,7 +134,7 @@ html_theme = 'default'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-# html_theme_path = []
+#html_theme_path = []
 
 # The name for this set of Sphinx documents.
 # "<project> v<release> documentation" by default.
@@ -171,7 +171,7 @@ html_static_path = ['_static']
 # bottom, using the given strftime format.
 # The empty string is equivalent to '%b %d, %Y'.
 #
-# html_last_updated_fmt = None
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -348,3 +348,8 @@ texinfo_documents = [
 
 # -- Options for Internationalization output ------------------------------
 locale_dirs = ['locale/']
+
+# openstackdocstheme options
+repository_name = 'openstack/tripleo-validations'
+bug_project = 'tripleo'
+bug_tag = 'documentation'
