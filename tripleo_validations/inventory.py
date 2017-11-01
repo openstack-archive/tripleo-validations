8d86b0dbfba90e77f615e704cd39f9042fe4fbf1
Steven Hardy <shardy@redhat.com>
2017-11-01 16:59:56 +0000

    Add per-network bind ips to inventory
    This adds the per-node per-network ips for each network so we
    can e.g set the bind ips for services deployed via ansible.
    
    Change-Id: I6085df4741d944875afbe02d71b9e9a85c406024

--- tripleo_validations/inventory.py
+++ tripleo_validations/inventory.py
@@ -170,6 +170,10 @@ class TripleoInventory(object):
                         ret[name]['vars'] = {
                             'deploy_server_id': role_node_id_map[
                                 'server_ids'][role][idx]}
+                    # Add variable for IP on each network
+                    for net in role_net_ip_map[role]:
+                        ret[name]['vars']["%s_ip" % net] = \
+                            role_net_ip_map[role][net][idx]
 
                 children.append(role)
                 ret[role] = {
