2d565ad8ee53ce14525abf8bf290ca3d84833884
Hervé Beraud <hberaud@redhat.com>
2020-06-02 21:05:42 +0200

    Stop to use the __future__ module.
    The __future__ module [1] was used in this context to ensure compatibility
    between python 2 and python 3.
    
    We previously dropped the support of python 2.7 [2] and now we only support
    python 3 so we don't need to continue to use this module and the imports
    listed below.
    
    Imports commonly used and their related PEPs:
    - `division` is related to PEP 238 [3]
    - `print_function` is related to PEP 3105 [4]
    - `unicode_literals` is related to PEP 3112 [5]
    - `with_statement` is related to PEP 343 [6]
    - `absolute_import` is related to PEP 328 [7]
    
    [1] https://docs.python.org/3/library/__future__.html
    [2] https://governance.openstack.org/tc/goals/selected/ussuri/drop-py27.html
    [3] https://www.python.org/dev/peps/pep-0238
    [4] https://www.python.org/dev/peps/pep-3105
    [5] https://www.python.org/dev/peps/pep-3112
    [6] https://www.python.org/dev/peps/pep-0343
    [7] https://www.python.org/dev/peps/pep-0328
    
    Change-Id: I91036721fcb78c8eec234577d3c4f40690fb6eb6

--- callback_plugins/validation_json.py
+++ callback_plugins/validation_json.py
@@ -13,8 +13,6 @@
 # implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-from __future__ import (absolute_import, division, print_function)
 __metaclass__ = type
 
 import datetime
--- library/docker_facts.py
+++ library/docker_facts.py
@@ -13,10 +13,6 @@
 # WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
 # License for the specific language governing permissions and limitations
 # under the License.
-
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
 import itertools
 
 from ansible.module_utils.basic import AnsibleModule
