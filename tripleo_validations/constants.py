b0f9444b770a359f4cdc62e104b3f45bab22f8f2
Mathieu Bultel <mat.bultel@gmail.com>
2020-12-11 22:50:08 +0100

    Move tripleo validations CLI from tripleoclient to tripleo-validations repo
    This patch is moving the CLI outside of tripleoclient to tripleo-validations
    repository as an additional tool on top of the main tripleo CLI.
    
    Change-Id: I3340fb3b038f1f081bddb26468e4aea9b1ab6883

---
+++ tripleo_validations/constants.py
@@ -0,0 +1,35 @@
+#   Copyright 2015 Red Hat, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#   not use this file except in compliance with the License. You may obtain
+#   a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#   WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#   License for the specific language governing permissions and limitations
+#   under the License.
+#
+
+import os
+
+DEFAULT_VALIDATIONS_BASEDIR = "/usr/share/ansible"
+DEFAULT_VALIDATIONS_LEGACY_BASEDIR = "/usr/share/openstack-tripleo-validations"
+
+VALIDATIONS_LOG_BASEDIR = '/var/log/validations'
+
+ANSIBLE_VALIDATION_DIR = (
+    os.path.join(DEFAULT_VALIDATIONS_LEGACY_BASEDIR, 'playbooks')
+    if os.path.exists(os.path.join(DEFAULT_VALIDATIONS_LEGACY_BASEDIR,
+                                   'playbooks'))
+    else "/usr/share/ansible/validation-playbooks"
+    )
+
+
+VALIDATION_GROUPS_INFO = (
+        '/usr/share/ansible/groups.yaml'
+        if os.path.exists('/usr/share/ansible/groups.yaml')
+        else os.path.join(DEFAULT_VALIDATIONS_LEGACY_BASEDIR, 'groups.yaml')
+        )
