d88808d0636f435cfc98c9638ecd57613fd19aae
David Vallee Delisle <dvd@redhat.com>
2021-07-08 18:41:09 -0400

    [ussuri-train-only] Use more recent version of ansible-lint
    we need fix[1] to bypass RepresenterError when using to_yaml filter.
    
    [1] https://github.com/ansible/ansible-lint/pull/518
    
    Change-Id: I8eead4d1a4ceac3d053af69e302c5b757c45cdaa

--- .ansible-lint_rules/ValidationHasMetadataRule.py
+++ .ansible-lint_rules/ValidationHasMetadataRule.py
@@ -1,5 +1,4 @@
 import os
-import six
 import yaml
 
 from ansiblelint import AnsibleLintRule
@@ -106,7 +105,7 @@ class ValidationHasMetadataRule(AnsibleLintRule):
                                 '*metadata* should contain a %s key' % info))
                             continue
                         if not isinstance(metadata.get(info),
-                                          six.string_types):
+                                          str):
                             results.append((
                                 {path: data},
                                 '*%s* should be a string' % info))
